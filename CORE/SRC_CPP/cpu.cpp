9c28d54f79456f15efb8b7750ed5bebf9c554ae3
Robin <robin.lembach@outlook.com>
2019-03-09 21:54:49 +0100

    INC R - INC D

--- CORE/SRC_CPP/cpu.cpp
+++ CORE/SRC_CPP/cpu.cpp
@@ -401,6 +401,23 @@ void Cpu::_nop()
     m_pc = m_pc + 1u;
 }
 
+void Cpu::_inc_r()
+{
+    // Variables locales
+    std::uint8_t 	w_registerMask      = 0u;
+    std::uint16_t*	wp_register16bits   = NULL;
+
+    // Récupération du registre 16b
+    w_registerMask = (mp_mpu->getMemVal(m_opcodeIdx) >> 4u);
+    _decodeRegister16Bits(w_registerMask, wp_register16bits);
+
+    // Incrémentation
+    ++(*wp_register16bits);
+
+    // Mise à jour de PC
+    m_pc += 1u;
+}
+
 void Cpu::_dec_r()
 {
     // Variables locales
@@ -418,6 +435,54 @@ void Cpu::_dec_r()
     m_pc += 1u;
 }
 
+void Cpu::_inc_d()
+{
+    // Variables locales
+    std::uint8_t 	w_registerMask      = 0u;
+    std::uint8_t*	wp_register8bits    = NULL;
+    std::uint16_t*	wp_register16bits   = NULL;
+    std::uint8_t    w_valueAtHL         = 0u;
+
+    // Récupération du registre 8b
+    w_registerMask = ((mp_mpu->getMemVal(m_opcodeIdx) & 0x38) >> 3u);
+    _decodeRegister8Bits(w_registerMask, wp_register8bits, wp_register16bits);
+
+    if (w_registerMask == 6u)
+    {
+        // Récupération de la valeur à l'adresse contenue par HL
+        w_valueAtHL = mp_mpu->getMemVal(*wp_register16bits);
+
+        // Gestion du flag H
+        m_registers.sFlags.h = static_cast<std::uint8_t>((w_valueAtHL & 0x0F) == 0x0F);
+
+        // Incrémentation de la valeur à l'adresse contenue par HL
+        mp_mpu->setMemVal(*wp_register16bits, ++w_valueAtHL);
+
+        // Gestion du flag Z
+        m_registers.sFlags.z = static_cast<std::uint8_t>(w_valueAtHL == 0u);
+
+        // Gestion du flag N
+        m_registers.sFlags.n = 1u;
+    }
+    else
+    {
+        // Gestion du flag H
+        m_registers.sFlags.h = static_cast<std::uint8_t>((*wp_register8bits & 0x0F) == 0x0F);
+
+        // Décrémentation du registre 8 bits
+        ++(*wp_register8bits);
+
+        // Gestion du flag Z
+        m_registers.sFlags.z = static_cast<std::uint8_t>(*wp_register8bits == 0u);
+
+        // Gestion du flag N
+        m_registers.sFlags.n = 1u;
+    }
+
+    // Mise à jour de PC
+    m_pc += 1u;
+}
+
 void Cpu::_dec_d()
 {
     // Variables locales
@@ -426,7 +491,7 @@ void Cpu::_dec_d()
     std::uint16_t*	wp_register16bits   = NULL;
     std::uint8_t    w_valueAtHL         = 0u;
 
-    // Récupération du registre 16b
+    // Récupération du registre 8b
     w_registerMask = ((mp_mpu->getMemVal(m_opcodeIdx) & 0x38) >> 3u);
     _decodeRegister8Bits(w_registerMask, wp_register8bits, wp_register16bits);
 
